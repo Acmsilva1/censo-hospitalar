2bb9f24c42168c3cc3244a7066cc83f7f1a2254d
Acmsilva1 <andrecarlos.miranda@gmail.com>
2026-05-01 19:49:29 -0300

    fix: alinha a altura das tags no topo do vidro para nao cortarem

--- apps/web-shell/src/HospitalFloorInterior3D.tsx
+++ apps/web-shell/src/HospitalFloorInterior3D.tsx
@@ -114,10 +114,10 @@ function SectorBlock({ sector, position, size, isBottomHalf }: { sector: Sector;
         <meshStandardMaterial color="#1a2e3f" roughness={0.8} />
       </mesh>
       
-      {/* Título do Setor empurrado para fora da caixa de vidro e mais baixo para "fugir" dos leitos */}
+      {/* Título do Setor empurrado para fora da caixa de vidro e alinhado ao topo para não cortar */}
       <SpriteLabel 
         text={sector.name} 
-        position={[0, 0.8, isBottomHalf ? d/2 + 1.6 : -d/2 - 1.6]} 
+        position={[0, 1.6, isBottomHalf ? d/2 + 1.2 : -d/2 - 1.2]} 
         scale={Math.max(1.2, w * 0.15)} 
       />
 
